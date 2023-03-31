6c2f2cacb511ee170059ae40f63917da4ada40df
Robert Moegenburg <r.moegenburg@t-online.de>
2023-03-31 21:52:07 +0200

    model and playercontroller, inputsystem

---
+++ MJG16/Assets/__Scripts/_Player/PlayerController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // input manager 
+    // movement WASD
+    // actions / actionmap
+    // functions mapControl
+    // Animation State // animator!!
+
+    // healtManage ?
+}
