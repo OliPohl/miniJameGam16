8ef0aa313c758720bfd9078aded310245a6aa02f
Robert Moegenburg <r.moegenburg@t-online.de>
2023-04-01 19:30:47 +0200

    checkPointSystem

--- MJG16/Assets/__Scripts/_Player/PlayerController.cs
+++ MJG16/Assets/__Scripts/_Player/PlayerController.cs
@@ -6,21 +6,22 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float PlayerDirection { get; set; }
-    public Transform PlayerTransform;
+    public Transform PlayerLastCheckPoint { get; set; }
     public static PlayerController Instance;
     private Animator animator;
     // PLAYER VARS
-    [SerializeField]private float FlipSpeedLerp = 5f;
+    [SerializeField] private float FlipSpeedLerp = 5f;
     private float FlipTimeStamp = 0f;
     private float FlipLerpDuration = 3.0f;
-    [SerializeField] private float speed=5.0f;
-    [SerializeField] private float jumpPower= 20.0f;
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float jumpPower = 20.0f;
     [SerializeField] private Transform groundcheck;
     [SerializeField] private Transform playerFlipModel;
     [SerializeField] private LayerMask groundLayer;
-    private float Gravity  = 9.81f;
-    private float MovementThresholdTimer=0.0f;
-    private bool isFacingRight = true;
+    [SerializeField] private Animator CameraBlendEffectAnimator;
+    private float Gravity = 9.81f;
+    private float MovementThresholdTimer = 0.0f;
+    private bool isFacingRight = false;
     public bool isJumping = false;
     private Rigidbody rb;
     private Quaternion localRot = Quaternion.Euler(0, 0, 0);
@@ -38,34 +39,39 @@ public class PlayerController : MonoBehaviour
     {
         animator = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody>();
-        PlayerTransform = gameObject.transform;
+        // PlayerTransform = gameObject.transform;
+        Flip();
+        PlayerLastCheckPoint =  new GameObject("StartCheckPoint").transform;
+        PlayerLastCheckPoint.position = gameObject.transform.position;
     }
     private void FixedUpdate()
     {
         Move();
         Animate();
         Flip();
-        
+
     }
     private void Move()
     {
-        rb.velocity = new Vector2(PlayerDirection*speed, rb.velocity.y);
+        rb.velocity = new Vector2(PlayerDirection * speed, rb.velocity.y);
     }
-    
+
     private void Animate()
     {
         /// FACING ANIMATION ///
-        if(PlayerDirection <-0.2f || PlayerDirection > 0.2f)
+        if (PlayerDirection < -0.2f || PlayerDirection > 0.2f)
         {
             animator.SetBool("isRunning", true);
-        }else{
+        }
+        else
+        {
             animator.SetBool("isRunning", false);
         }
         /// JUMPING ANIMATION ///
-        if(isJumping)
+        if (isJumping)
         {
             animator.SetTrigger("Jumping");
-            isJumping=false;
+            isJumping = false;
         }
         // if jumping trigger jump
         // if pickup trigger pickup
@@ -76,44 +82,45 @@ public class PlayerController : MonoBehaviour
     }
     private bool isGrounded()
     {
-        return (Physics.OverlapBox(groundcheck.position, transform.localScale / 4, Quaternion.identity, groundLayer).Length>0);
+        return (Physics.OverlapBox(groundcheck.position, transform.localScale / 4, Quaternion.identity, groundLayer).Length > 0);
     }
     private void Flip()
     {
-        if(!isFacingRight && PlayerDirection >0f)
+        if (!isFacingRight && PlayerDirection > 0f)
         {
             isFacingRight = !isFacingRight;
-            
-            localRot = Quaternion.Euler(0f,90f,0f);
+
+            localRot = Quaternion.Euler(0f, 90f, 0f);
             FlipTimeStamp = 0f;
-            
-        } else if(isFacingRight && PlayerDirection < 0f)
-        {
-            isFacingRight = !isFacingRight;
-            localRot = Quaternion.Euler(0f,-90f,0f);
-            FlipTimeStamp = 0f;
-            
-            
+
         }
-        
-        playerFlipModel.localRotation = Quaternion.Lerp(playerFlipModel.rotation,localRot,FlipTimeStamp*FlipSpeedLerp / FlipLerpDuration);
+        else if (isFacingRight && PlayerDirection < 0f)
+        {
+            isFacingRight = !isFacingRight;
+            localRot = Quaternion.Euler(0f, -90f, 0f);
+            FlipTimeStamp = 0f;
+
+
+        }
+
+        playerFlipModel.localRotation = Quaternion.Lerp(playerFlipModel.rotation, localRot, FlipTimeStamp * FlipSpeedLerp / FlipLerpDuration);
         FlipTimeStamp += Time.fixedDeltaTime;
-    
-        
+
+
     }
     public void PlayerJump(float jumpModifier)
     {
-       
-       if(isGrounded())
-       {
-           
+
+        if (isGrounded())
+        {
+
             isJumping = true;
             Vector3 vel = transform.up * jumpPower;
             rb.AddForce(rb.velocity + vel, ForceMode.Impulse);
-       }
-       if(jumpModifier == 0.5f)
+        }
+        if (jumpModifier == 0.5f)
         {
-           
+
             isJumping = false;
             // rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jumpPower* jumpModifier);
         }
@@ -122,5 +129,36 @@ public class PlayerController : MonoBehaviour
     // functions mapControl
     // Animation State // animator!!
 
-    // healtManage ?
+    private void OnDeath()
+    {
+        RespawnTransition();
+        RespawnPlayer();
+        // respawn to lastCheckPoint
+        // reset FaceOrientation
+        isFacingRight = false;
+        Flip();
+
+    }
+    private void RespawnPlayer()
+    {
+        gameObject.transform.position = PlayerLastCheckPoint.position;
+    }
+    private void RespawnTransition()
+    {
+        // EYES CLOSED EFFECT 
+        CameraBlendEffectAnimator.Play("RespawnCameraBlending");
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Death"))
+        {
+            OnDeath();
+        }
+        if (other.CompareTag("CheckPoint"))
+        {
+            PlayerLastCheckPoint = other.gameObject.transform;
+            Debug.Log("CheckPointSystem: Updated - New CheckPoint");
+        }
+    }
 }
