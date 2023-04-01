704a4e75d0954d95c76a2ddd3ad65b30f1cf7f10
OliPohl <oliver@pohl66.de>
2023-04-01 21:47:20 +0200

    scissors

---
+++ MJG16/Assets/__Scripts/ObjectInteract/ScissorsInteract.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class ScissorsInteract : MonoBehaviour, IInteractable
+{
+    private Vector3 startPos;
+    void Start() {
+        transform.position = startPos;
+    }
+
+    public string Data()
+    {
+        if(PlayerInventory.Instance.Item == gameObject)
+            return "Drop Scissors";
+        else
+            return "Pickup Scissors";
+    }
+
+    public void Interact()
+    {
+        if(PlayerInventory.Instance.Item == gameObject)
+            PlayerInventory.Instance.TakeObject(null);
+        else
+            PlayerInventory.Instance.TakeObject(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Death")
+        {
+            ResetPos();
+        }
+    }
+
+    private void ResetPos()
+    {
+        transform.SetParent(null);
+        transform.position = startPos;
+    }
+}
