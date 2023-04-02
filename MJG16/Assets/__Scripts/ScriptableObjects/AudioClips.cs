936a717a7dd9d0c29f71d3af6d5c3e3c404bd592
OliPohl <oliver@pohl66.de>
2023-04-02 13:39:38 +0200

    audio imported

--- MJG16/Assets/__Scripts/ScriptableObjects/AudioClips.cs
+++ MJG16/Assets/__Scripts/ScriptableObjects/AudioClips.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AudioClips", menuName = "MJG16/AudioClips", order = 0)]
 public class AudioClips : ScriptableObject
 {
-    [field: Header("Menu")]
-    [field: SerializeField] public AudioClip ButtonPress { get; private set; }
+    [field: SerializeField] public AudioClip[] Footsteps { get; private set; }
+    [field: SerializeField] public AudioClip book { get; private set; }
+    [field: SerializeField] public AudioClip breaking { get; private set; }
+    [field: SerializeField] public AudioClip ducttape { get; private set; }
+    [field: SerializeField] public AudioClip fall { get; private set; }
+    [field: SerializeField] public AudioClip fire { get; private set; }
+    [field: SerializeField] public AudioClip achievement { get; private set; }
+    [field: SerializeField] public AudioClip wind { get; private set; }
 }
